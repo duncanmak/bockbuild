db8bb8805b74e922a7628de7c5a59e845cdb4014
Duncan Mak <duncan.mak@xamarin.com>
2012-02-01 21:33:37 -0500

    Install only the files that are needed.

--- packages/libjpeg.py
+++ packages/libjpeg.py
@@ -1,6 +1,8 @@
-Package ('libjpeg', '8',
-	source_dir_name = 'jpeg-%{version}',
-	sources = [
-		'http://www.ijg.org/files/jpegsrc.v%{version}.tar.gz'
-	]
-)
+class LibJpegPackage (Package):
+	def __init__ (self):
+		Package.__init__(self, 'libjpeg', '8', sources = ['http://www.ijg.org/files/jpegsrc.v8.tar.gz'])
+		self.source_dir_name = 'jpeg-8'
+		self.makeinstall = 'make install-libLTLIBRARIES'
+
+LibJpegPackage()
+
