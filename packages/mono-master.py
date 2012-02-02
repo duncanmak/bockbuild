42c214c89dd4800b509ec91795d89f766294265a
Duncan Mak <duncan.mak@xamarin.com>
2012-02-01 21:35:49 -0500

    Fix up pkg-config and add the DESTDIR install trick to mono-master.

--- packages/mono-master.py
+++ packages/mono-master.py
@@ -1,28 +1,44 @@
 class MonoMasterPackage(Package):
+	OSX_FRAMEWORK_PATH = "/Library/Frameworks/Mono.framework/Versions/2.11/"
+
 	def __init__(self):
 		Package.__init__(self, 'mono', '2.11',
-                     sources = ['git://github.com/mono/mono'],
-                     configure_flags = ['--enable-nls=no',
-                                        ]
+										 sources = ['git://github.com/mono/mono'],
+										 configure_flags = ['--enable-nls=no',
+																				'--prefix=' + self.OSX_FRAMEWORK_PATH,
+																				],
+										 source_dir_name = "mono-2.11.git"
                      )
 		if Package.profile.name == 'darwin':
 			self.configure_flags.extend([
-          # fix build on lion, it uses 64-bit host even with -m32
-          '--build=i386-apple-darwin11.2.0',
-          ])
+					# fix build on lion, it uses 64-bit host even with -m32
+					'--build=i386-apple-darwin11.2.0',
+					])
+			self.sources.extend(['patches/pkg-config'])
 
-		self.configure = './autogen.sh --prefix="%{prefix}"'
-    
-		# Mono(in libgc) likes to fail to build randomly
-		self.make = 'make'
-
-#	def prep(self):
-#		Package.prep(self)
-#		self.sh('patch -p1 < "%{sources[1]}"')
+		self.configure = './autogen.sh'
+		self.makeinstall = 'make install DESTDIR="%{prefix}"'
 
 	def install(self):
 		Package.install(self)
 		if Package.profile.name == 'darwin':
-			self.sh('sed -ie "s/libcairo.so.2/libcairo.2.dylib/" "%{prefix}/etc/mono/config"')
+			self.fixup_pkgconfig ()
+			self.sh('rsync -azP %{prefix}/Library/Frameworks/Mono.framework/Versions/2.11/* %{prefix}')
+			self.sh('rm -rf %{prefix}/Library/')
+
+	def fixup_pkgconfig(self):
+		if not os.path.exists("%s/bin/pkg-config.bin" % self.prefix):
+			self.sh('cp %{prefix}/bin/pkg-config %{prefix}/bin/pkg-config.bin')
+			
+		pkgconfig = os.path.join(self.package_dest_dir(), os.path.basename(self.sources[1]))
+		temp = pkgconfig + ".tmp"
+
+		with open(pkgconfig) as input:
+			with open(temp, "w") as output:
+				for line in input:
+					output.write(re.sub('VERSION="REPLACE ME"', 'VERSION="2.11"', line))
+
+		os.rename(temp, pkgconfig)
+		self.sh('cp %s %s' % (pkgconfig, '%{prefix}/bin/pkg-config'))
 
 MonoMasterPackage()
